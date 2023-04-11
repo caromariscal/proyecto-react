3b65ac0fa9442baf6c933919cd8635042398c6a1
caro <cmariscal2001@gmail.com>
2023-04-11 18:13:59 -0300

    segundo arreglo

--- src/routes/PrivateRoutes.js
+++ src/routes/PrivateRoutes.js
@@ -1,9 +1,9 @@
 import { ItemListContainer } from "../componentes/ItemListContainer/ItemListContainer";
-import { Navbar } from "../componentes/navbar/Navbar";
+import { Navbar } from "../componentes/Navbar/Navbar";
 import { ItemDetailContainer } from "../componentes/ItemDetailContainer/ItemDetailContainer";
 import { Cart } from "../componentes/Cart/Cart";
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { Checkout } from "../componentes/Checkout/checkout";
+import { Checkout } from "../componentes/Checkout/Checkout";
 
 
 export const PrivateRoutes = () => {
