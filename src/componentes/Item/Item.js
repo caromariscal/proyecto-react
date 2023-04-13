c770f6a22c22769360607bc7997bd06a4af5bdb3
caro <cmariscal2001@gmail.com>
2023-04-13 15:51:08 -0300

    arreglo de las imagenes

--- src/componentes/Item/Item.js
+++ src/componentes/Item/Item.js
@@ -1,10 +1,11 @@
+import './items.scss'
 import { Link } from "react-router-dom"
 
 export const Item = ( {item} ) => {
 
     return (
-        <div className='col-3 m-1'>
-            <img src={item.img} alt={item.name}/>
+        <div className='col-3 m-1 text-center'>
+            <img className="imagen" src={item.img} alt={item.name}/>
             <h4>{item.name}</h4>
             <p>{item.description}</p>
             <p>Precio: <strong>${item.price}</strong></p>
